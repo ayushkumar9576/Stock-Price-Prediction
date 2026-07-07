9ab202c3b7ddcd6f857687d356d5fd41f79fa8b0
ayushkumar9576 <ayushmth9576@gmail.com>
2026-07-07 14:14:52 +0530

    Added code for loading the data

---
+++ src/data_loader.py
@@ -0,0 +1,73 @@
+import os
+import logging
+from abc import ABC, abstractmethod
+import pandas as pd
+import yfinance as yf
+
+logger = logging.getLogger(__name__)
+
+class BaseDataLoader(ABC):
+    @abstractmethod
+    def load(self, ticker: str, start: str, end: str)-> pd.DataFrame:
+        pass
+
+class YFinanceLoader(BaseDataLoader):
+
+    def __init__(self, raw_dir: str)-> None:
+        logger.info("Downloading and caching the stock price data from Yahoo finance")
+        self._raw_dir = raw_dir
+    
+    def load(self, ticker: str, start: str, end: str)-> pd.DataFrame:
+        os.makedirs(self._raw_dir, exist_ok=True)
+        cache_path = os.path.join(self._raw_dir, f"{ticker}.csv")
+
+        if os.path.exists(cache_path):
+            logger.info(f"[{ticker}] Loading the data from the cache file")
+            return pd.read_csv(cache_path, index_col="Date", parse_dates=True)
+        
+        logger.info(f"[{ticker}] downloading the data from the Yahho Finance between ({start} to {end})")
+
+        df = yf.download(ticker, start, end, auto_adjust=True)
+
+        if isinstance(df, pd.MultiIndex):
+            df.columns = df.columns.get_level_values(0)
+
+        if df.empty:
+            raise ValueError(f"No data returned for {ticker} between {start} and {end}")
+        
+        df.to_csv(cache_path)
+        logger.info(f"Downloaded the data for {ticker} between {start} and {end}")
+
+class CSVLoader(BaseDataLoader):
+    def __init__(self, raw_dir: str):
+        logger.info("Downloading the stock data from a pre-loaded CSV")
+        self._raw_dir = raw_dir
+    
+    def load(self, ticker: str, start: str, end: str) -> pd.DataFrame:
+        csv_path = os.path.join(self._raw_dir, f"{ticker}.csv")
+
+        if not os.path.exists(csv_path):
+            raise FileNotFoundError(f"No cached CSV found at {csv_path}. Run with YFinanceLoader first to download and cache the data.")
+
+        logger.info(f"[{ticker}] Loading from CSV: {csv_path}")
+        df = pd.read_csv(csv_path, index_col="Date", parse_dates=True)
+        df = df.loc[start:end]
+
+        if df.empty:
+            raise ValueError(f"CSV for '{ticker}' has no data between {start} and {end}.")
+
+        logger.info(f"[{ticker}] Loaded {len(df)} rows from CSV.")
+        return df
+
+class DataLoader:
+    def __init__(self, strategy: BaseDataLoader)-> None:
+        logger.info("Setting the strategy for data loading")
+        self._strategy = strategy
+    
+    def set_strategy(self, strategy: BaseDataLoader)-> None:
+        logger.info("Changing the startegy for loading the data")
+        self._strategy = strategy
+    
+    def load(self, ticker: str, start: str, end: str)-> pd.DataFrame:
+        logger.info("Loading the data using the selected strategy")
+        return self._strategy.load(ticker, start, end)
